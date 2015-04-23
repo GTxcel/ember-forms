2e1f3dfb83eea5d2087ef2c6d1199c38c0b60a59
pscannell <pscannell@gtxcel.com>
2015-04-23 10:22:43 -0400

    Add capability to specify default in Select control. For now, only "selection" option works (see Ember.Select documentation)

--- addon/select.js
+++ addon/select.js
@@ -4,14 +4,14 @@ import ControlMixin from 'ember-idx-forms/mixins/control';
 import Utils from 'ember-idx-forms/utils/utils';
 
 /*
-Form Select
+ Form Select
 
-Syntax:
-{{em-select property="property name"
-    content=array_of_options
-    optionValuePath=keyForValue
-    optionLabelPath=keyForLabel
-    prompt="Optional default prompt"}}
+ Syntax:
+ {{em-select property="property name"
+ content=array_of_options
+ optionValuePath=keyForValue
+ optionLabelPath=keyForLabel
+ prompt="Optional default prompt"}}
  */
 export default FormGroupComponent.extend({
   v_icons: false,
@@ -22,13 +22,17 @@ export default FormGroupComponent.extend({
     optionValuePath: Em.computed.alias('parentView.optionValuePath'),
     optionLabelPath: Em.computed.alias('parentView.optionLabelPath'),
     prompt: Em.computed.alias('parentView.prompt'),
-    multiple: Em.computed.alias('parentView.multiple')
+    multiple: Em.computed.alias('parentView.multiple'),
+    value: Em.computed.alias('parentView.value'),
+    selection: Em.computed.alias('parentView.selection')
   }),
   property: void 0,
   content: void 0,
   optionValuePath: void 0,
   optionLabelPath: void 0,
   prompt: void 0,
+  value: void 0,
+  selection: void 0,
   controlWrapper: (function() {
     if (this.get('form.form_layout') === 'horizontal') {
       return Utils.getFormsColsClass(this.get('form.form_layout_columns'), "left", "false");
